d2413b72ae39c0cc34082de08b17b91f9128e58e
rozalinutza22 <rozalinutza@protonmail.com>
2024-05-28 21:58:42 +0300

    Create a config file for the database

---
+++ includes/config_db.php
@@ -0,0 +1,12 @@
+<?php
+    $mysql = new mysqli (
+        'localhost', 
+        'root',       
+        '',    
+        'cupo_db'  
+        );
+
+    if (mysqli_connect_errno()) {
+        die ('Failed to connect to MySql database...');
+    }
+
