e231a64c3ef561e8d4fe4fce40bd9078b14ac08d
rozalinutza22 <rozalinutza@protonmail.com>
2024-05-31 00:45:42 +0300

    Modified the structure

--- controllers/create_account_controller.php
+++ controllers/create_account_controller.php
@@ -1,7 +1,9 @@
 <?php
 include(dirname(__DIR__).'/models/create_account_model.php');
-include(dirname(__DIR__).'/views/create_account_view.php');
+include(dirname(__DIR__).'/views/create_account.php');
 
 require __DIR__ . "/includes/config_db.php";
+
+
 
 ?>
