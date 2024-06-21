9e22e6cea99d610cb907a44a14f6072e430e5dd1
rozalinutza22 <rozalinutza@protonmail.com>
2024-06-21 12:44:08 +0300

    Now curl http://cupo/api/ prints a json file containing a short message

--- public/api.php
+++ public/api.php
@@ -1,14 +1,11 @@
 <?php
 
-// header('Content-Type: application/json');
+header('Content-Type: application/json');
 
-// $data = [
-//     'message' => 'hello'
-// ];
+$data = [
+    'message' => 'hello'
+];
 
-// echo json_encode($data);
-    // http://cupo:81/api.php
-
-    echo "hei";
+echo json_encode($data);
 
 ?>
