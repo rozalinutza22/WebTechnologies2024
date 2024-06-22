399f1e2619756e113f97576fdf9171d2806c473c
rozalinutza22 <rozalinutza@protonmail.com>
2024-06-22 15:30:47 +0300

    Added the allowed methods(for the moment)

--- public/UserManagement.php
+++ public/UserManagement.php
@@ -36,7 +36,10 @@
             $products = $result->fetch_all(MYSQLI_ASSOC);
 
             $stmt->close();
-            return $products;
+            
+            if (!empty($products)) {
+                return $products;
+            }else return $emp = "User does not exist in the database";
         }
 
         public function create($data) {
@@ -76,6 +79,14 @@
                     break;
                 case "POST":
                     $data = (array) json_decode(file_get_contents("php://input"), true);
+                    $errors = $this->getValidationErrors($data);
+
+                    if (!empty($errors)) {
+                        http_response_code(422); //Unprocessable Entityt
+                        echo json_encode(["errors" => $errors]);
+                        break;
+                    }
+
                     $this->create($data);
 
                     http_response_code(201); //Created Successfully 
@@ -83,8 +94,34 @@
                         "message" => "User created"
                     ]);
                     break;
+
+                default: 
+                    //Method Not Allowed
+                    http_response_code(405); 
+                    header("Allow: GET, POST");
+                    
+                    echo "Method not allowed.\n";
+                    echo "Allow: GET, POST";
             }
 
+        }
+
+        private function getValidationErrors($data) {
+            $errors = [];
+
+            if (array_key_exists("admin", $data)) {
+                if (filter_var($data["admin"], FILTER_VALIDATE_INT) === false) {
+                    $errors[] = "admin must be an integer";
+                }
+            }else echo "nnue";
+
+            if (array_key_exists("vegetarian", $data)) {
+                if (filter_var($data["vegetarian"], FILTER_VALIDATE_INT) === false) {
+                    $errors[] = "vegetarian must be an integer";
+                }
+            }else echo "nue";
+
+            return $errors;
         }
     }
 ?>
